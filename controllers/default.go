b91dd5172986768fdd66c37cd7f3d37c2c37cd99
jhonkus <putukn@gmail.com>
2019-05-14 16:47:00 +0800

    remove unused data

--- controllers/default.go
+++ controllers/default.go
@@ -8,17 +8,15 @@ type MainController struct {
 	beego.Controller
 }
 
-//Get ...
-func (c *MainController) Get() {
-	c.Data["Website"] = "spinechain.go"
-	c.Data["Email"] = "admin@spineshain.com"
-	c.TplName = "index.tpl"
+//Get is login page
+func (main *MainController) Get() {
+	main.Data["title"] = "Login - Register"
+	main.TplName = "index.tpl"
 }
 
+//Dashboard main dashboard
 func (main *MainController) Dashboard() {
+	main.Data["title"] = "Dashboard"
 	main.Layout = "admin.html"
-	main.Data["Website"] = "My Website"
-	main.Data["Email"] = "your.email.address@example.com"
-	main.Data["EmailName"] = "Your Name"
 	main.TplName = "default/dashboard.tpl"
 }
