333bfb12cb1aa6fb46d7111fb599ae4f63cebff9
jhonkus <putukn@gmail.com>
2019-05-14 13:53:20 +0800

    Add Send Message

--- controllers/account.go
+++ controllers/account.go
@@ -4,18 +4,35 @@ import (
 	"github.com/astaxie/beego"
 )
 
+//AccountController ...
 type AccountController struct {
 	beego.Controller
 }
 
+//AddAccountForm ...
 func (acc *AccountController) AddAccountForm() {
 	acc.Data["Website"] = "spinechain.go"
 	acc.Data["Email"] = "admin@spineshain.com"
 	acc.TplName = "account/index.tpl"
 }
 
+//SendMoneyForm ...
 func (acc *AccountController) SendMoneyForm() {
 	acc.Data["Website"] = "spinechain.go"
 	acc.Data["Email"] = "admin@spineshain.com"
 	acc.TplName = "account/sendmoney.tpl"
+}
+
+//SendMessageForm ...
+func (acc *AccountController) SendMessageForm() {
+	acc.Data["Website"] = "spinechain.go"
+	acc.Data["Email"] = "admin@spineshain.com"
+	acc.TplName = "account/sendmessage.tpl"
+}
+
+//TransferHistoryTable ...
+func (acc *AccountController) TransferHistoryTable() {
+	acc.Data["Website"] = "spinechain.go"
+	acc.Data["Email"] = "admin@spineshain.com"
+	acc.TplName = "account/transferhistory.tpl"
 }
--- routers/router.go
+++ routers/router.go
@@ -9,5 +9,7 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/dashboard", &controllers.MainController{}, "get:Dashboard")
 	beego.Router("/sendmoney", &controllers.AccountController{}, "get:SendMoneyForm")
+	beego.Router("/sendmessage", &controllers.AccountController{}, "get:SendMessageForm")
 	beego.Router("/newaccount/", &controllers.AccountController{}, "get:AddAccountForm")
+	beego.Router("/transferhistory/", &controllers.AccountController{}, "get:TransferHistoryTable")
 }
