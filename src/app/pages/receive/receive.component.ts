364769d2fd7a60ead314f27781a2974876ae979e
bagasAgastya <bagasagastya19@gmail.com>
2019-06-03 15:41:02 +0800

    convert public key and store to local storage

--- src/app/pages/receive/receive.component.ts
+++ src/app/pages/receive/receive.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ReceiveComponent implements OnInit {
 
   // value for QR Code
-  address = 'BCZEGOb3WNx3fDOVf9ZS4EjvOIv';
+  address = localStorage.getItem('publicKey');;
 
   constructor() { }
 
