8e0d4d44ab279f86dfe4238f507c95e16f0f4cfc
jhonkus <putukn@gmail.com>
2019-06-03 08:37:04 +0800

    Add  12 Words Mnemonic  generator

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { Router } from "@angular/router";
+import { wordlist} from '../../../assets/js/wordlist';
 
 @Component({
   selector: "app-signup",
@@ -8,20 +9,7 @@ import { Router } from "@angular/router";
   styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit {
-  phaseprase = [
-    "yeah",
-    "howl",
-    "space",
-    "allow",
-    "bruise",
-    "essence",
-    "feed",
-    "demon",
-    "content",
-    "manage",
-    "beautiful",
-    "creature"
-  ];
+  phaseprase: any;
 
   constructor(private modalService: NgbModal, private router: Router) {}
 
@@ -31,11 +19,37 @@ export class SignupComponent implements OnInit {
   }
 
   generateNewPassphrase() {
+
+    const crypto = window.crypto;
+    let pass: any;
+    const phraseWords = [];
+
+    if (crypto) {
+      const bits = 128;
+      const random = new Uint32Array(bits / 32);
+      crypto.getRandomValues(random);
+      const n = wordlist.length;
+      let x: any;
+      let w1: any;
+      let w2: any;
+      let w3: any;
+
+      for (let i = 0; i < random.length; i++) {
+        x = random[i];
+        w1 = x % n;
+        w2 = (((x / n) >> 0) + w1) % n;
+        w3 = (((((x / n) >> 0) / n) >> 0) + w2) % n;
+        phraseWords.push(wordlist[w1]);
+        phraseWords.push(wordlist[w2]);
+        phraseWords.push(wordlist[w3]);
+      }
+    }
+    this.phaseprase = phraseWords;
     return this.phaseprase.sort(() => Math.random() - 0.5);
   }
 
   copyPassphrase() {
-    let phaseprase = this.phaseprase.join(" ");
+    const phaseprase = this.phaseprase.join(" ");
     this.copyText(phaseprase);
   }
 
