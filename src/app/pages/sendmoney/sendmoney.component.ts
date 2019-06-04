3b02ec6630d8046a273483c9c881610043027933
gungdesurya <gungde.avenger@gmail.com>
2019-06-04 18:54:29 +0800

    fetch send money

--- src/app/pages/sendmoney/sendmoney.component.ts
+++ src/app/pages/sendmoney/sendmoney.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2'
+
+import { AccountService } from '../../services/account.service'
+import { AppService } from '../../app.service'
 
 @Component({
   selector: 'app-sendmoney',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SendmoneyComponent implements OnInit {
 
-  constructor() { }
+  formSend: FormGroup
+  recipientForm = new FormControl("", Validators.required)
+  amountForm = new FormControl("", Validators.required)
+  feeForm = new FormControl("", Validators.required)
+  passPhraseForm = new FormControl("", Validators.required)
+
+  pubKey: string
+
+  constructor(private accountServ: AccountService, private appServ: AppService) {
+    this.formSend = new FormGroup({
+      recipient: this.recipientForm,
+      amount: this.amountForm,
+      fee: this.feeForm,
+      passphrase: this.passPhraseForm
+    })
+
+    appServ.currAccount.subscribe(pubKey => this.pubKey = pubKey)
+  }
 
   ngOnInit() {
+  }
+
+  onSendMoney() {
+    if (this.formSend.valid) {
+      let data = {
+        ...this.formSend.value,
+        from: this.pubKey,
+        senderPublicKey: 'Send Pub Key',
+        signatureHash: 'Sig Hash'
+      }
+      Swal.fire({
+        title: `Are you sure want to send money?`,
+        showCancelButton: true,
+        showLoaderOnConfirm: true,
+        preConfirm: () => {
+          return this.accountServ
+            .sendMoney(data)
+            .toPromise().then((res: any) => {
+              Swal.fire('Money sent')
+              return false
+            })
+            .catch(res => {
+              Swal.fire({ html: res.error.error, type: 'error' })
+              return false
+            })
+        },
+      })
+    }
   }
 
 }
--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,25 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { HttpClient } from '@angular/common/http';
-import { AppService } from '../app.service'
-
+import { HttpClient } from "@angular/common/http";
+import { AppService } from "../app.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AccountService {
-AccountTransaction = [];
-PublicKey : string = ''
-  constructor(
-    private http: HttpClient,
-    private appServ: AppService
-  ) {
+  AccountTransaction = [];
+  PublicKey: string = "";
+  apiUrl = "http://54.254.196.180:8000";
+
+  constructor(private http: HttpClient, private appServ: AppService) {
     appServ.currAccount.subscribe(account => {
-        this.PublicKey = account
-      })
+      this.PublicKey = account;
+    });
   }
   getAccountTransaction() {
-    return this.http.get(`http://54.254.196.180:8000/getAccountTransactions/${this.PublicKey}`);
+    return this.http.get(
+      `${this.apiUrl}/getAccountTransactions/${this.PublicKey}`
+    );
   }
 
+  sendMoney(data) {
+    return this.http.post(`${this.apiUrl}/sendMoney`, data);
+  }
 }
