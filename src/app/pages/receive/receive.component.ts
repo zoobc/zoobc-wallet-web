84c8d9f06cf2de0051e26393a1b4d76b9a898af1
bagasAgastya <bagasagastya19@gmail.com>
2019-06-28 10:52:25 +0800

    Add copy button for addres on receive page

--- src/app/pages/receive/receive.component.ts
+++ src/app/pages/receive/receive.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from '../../app.service'
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'app-receive',
   templateUrl: './receive.component.html',
-  styleUrls: ['./receive.component.css']
+  styleUrls: ['./receive.component.css'],
 })
 export class ReceiveComponent implements OnInit {
-
   // value for QR Code
-  address: string = ''
+  address: string = '';
 
   constructor(private appServ: AppService) {
-    this.address = appServ.currAddress
+    this.address = appServ.currAddress;
   }
 
-  ngOnInit() {
+  copyAddress() {
+    this.copyText(this.address);
+  }
+  copyText(text) {
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.opacity = '0';
+    selBox.value = text;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
   }
 
+  ngOnInit() {}
 }
