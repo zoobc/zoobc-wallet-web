5e23a6dc32ee512cbea3966f2284f8c151863c69
gungdesurya <gungde.avenger@gmail.com>
2019-07-19 16:40:49 +0800

    deleting unnecessary files (#78)

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,38 +1,7 @@
-import * as sha256 from "sha256";
-import { eddsa as EdDSA } from "elliptic";
-import * as bip39 from "../assets/js/bip39";
+import * as sha256 from 'sha256';
+import { eddsa as EdDSA } from 'elliptic';
 
-import { toBase64Url, stringToByteArray } from "../helpers/converters";
-
-export function generatePhraseWords(): string {
-  const mnemonic = bip39.generateMnemonic();
-  return mnemonic;
-
-  // const crypto = window.crypto;
-  // const phraseWords = [];
-
-  // if (crypto) {
-  //   const bits = 128;
-  //   const random = new Uint32Array(bits / 32);
-  //   crypto.getRandomValues(random);
-  //   const n = wordlist.length;
-  //   let x: any;
-  //   let w1: any;
-  //   let w2: any;
-  //   let w3: any;
-
-  //   for (let i = 0; i < random.length; i++) {
-  //     x = random[i];
-  //     w1 = x % n;
-  //     w2 = (((x / n) >> 0) + w1) % n;
-  //     w3 = (((((x / n) >> 0) / n) >> 0) + w2) % n;
-  //     phraseWords.push(wordlist[w1]);
-  //     phraseWords.push(wordlist[w2]);
-  //     phraseWords.push(wordlist[w3]);
-  //   }
-  // }
-  // return phraseWords;
-}
+import { toBase64Url, stringToByteArray } from '../helpers/converters';
 
 export function VerifySignature(key, signature, data) {
   const result = key.verify(data, signature);
@@ -47,7 +16,7 @@ export function GetSignature(key, data): any {
 // GetAddressFromPublicKey Get the formatted address from a raw public key
 export function GetAddressFromPublicKey(publicKey: Uint8Array): string {
   const checksum = GetChecksumByte(publicKey);
-  let binary = "";
+  let binary = '';
   const bytes = new Uint8Array(33);
   bytes.set(publicKey, 0);
   bytes.set([checksum[0]], 32);
@@ -74,7 +43,7 @@ export function GetSeedFromPhrase(phrase: string) {
 }
 
 export function GetKeyPairFromSeed(seed: ArrayLike<number>): any {
-  const ec = new EdDSA("ed25519");
+  const ec = new EdDSA('ed25519');
   const keyPair = ec.keyFromSecret(seed);
   return keyPair;
 }
