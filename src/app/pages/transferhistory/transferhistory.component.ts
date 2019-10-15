c3be8c6f4d21eda57f7bde975590e8fb24b9e12b
gungdesurya <gungde.avenger@gmail.com>
2019-10-15 14:37:41 +0800

    transfer history pagination bug fixed

--- src/app/pages/transferhistory/transferhistory.component.ts
+++ src/app/pages/transferhistory/transferhistory.component.ts
@@ -21,53 +21,52 @@ export class TransferhistoryComponent implements OnInit {
   total: number = 0;
   finished: boolean = false;
 
-  address: string;
+  address: string = this.authServ.getCurrAccount().address;
   isLoading: boolean = false;
   isError: boolean = false;
 
   constructor(
     private transactionServ: TransactionService,
     private authServ: AuthService
-  ) {
-    this.address = this.authServ.currAddress;
-  }
+  ) {}
 
   ngOnInit() {
     this.getTx(true);
-    // 72 is transaction item's height
-    this.perPage = Math.ceil(window.outerHeight / 72);
   }
 
   getTx(reload: boolean = false) {
     if (!this.isLoading) {
-      if (reload) this.accountHistory = null;
+      // 72 is transaction item's height
+      const perPage = Math.ceil(window.outerHeight / 72);
+
+      if (reload) {
+        this.accountHistory = null;
+        this.page = 1;
+      }
 
       this.isLoading = true;
       this.isError = false;
 
       this.transactionServ
-        .getAccountTransaction(this.page, this.perPage, this.address)
+        .getAccountTransaction(this.page, perPage, this.address)
         .then((res: Transactions) => {
-          if (!reload)
-            this.accountHistory = this.accountHistory.concat(res.transactions);
-          else this.accountHistory = res.transactions;
           this.total = res.total;
-          this.isLoading = false;
-        })
-        .then(() => {
-          // if relaad button pressed app will req unconfirmed tx too
+
           if (reload) {
-            this.transactionServ
-              .getUnconfirmTransaction(this.address)
-              .then((res: Transaction[]) => {
-                this.unconfirmTx = res;
-              });
-          }
+            this.accountHistory = res.transactions;
+            return this.transactionServ.getUnconfirmTransaction(this.address);
+          } else
+            this.accountHistory = this.accountHistory.concat(res.transactions);
+        })
+        .then((unconfirmTx: Transaction[]) => {
+          // if relaad button pressed app will req unconfirmed tx too
+          if (unconfirmTx) this.unconfirmTx = unconfirmTx;
         })
         .catch(() => {
-          this.isLoading = false;
           this.isError = true;
-        });
+          this.unconfirmTx = null;
+        })
+        .finally(() => (this.isLoading = false));
     }
   }
 
