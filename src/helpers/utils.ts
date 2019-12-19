6a515e1afe942ec7f65dc5c3c6a717b379be91b4
bagasAgastya <bagasagastya19@gmail.com>
2019-12-19 11:57:32 +0800

    fix copy button doesnt work on iphone x

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -40,13 +40,23 @@ export function generateEncKey(pin: string): string {
 }
 
 export function onCopyText(text: string) {
+  let isiOSDevice = navigator.userAgent.match(/ipad|iphone/i);
   let selBox = document.createElement('textarea');
+  let range = document.createRange();
+  let selection;
   selBox.style.position = 'fixed';
   selBox.style.opacity = '0';
   selBox.value = text;
   document.body.appendChild(selBox);
   selBox.focus();
   selBox.select();
+  if (isiOSDevice) {
+    range.selectNodeContents(selBox);
+    selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    selBox.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+  }
   document.execCommand('copy');
   document.body.removeChild(selBox);
 }
