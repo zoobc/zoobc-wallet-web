3e3a1035465fb6c828fe4e9ba96f447cecc9a167
I Made Witarsana <made.witarsana@gmail.com>
2020-04-08 10:22:53 +0800

    Add input-address component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -72,6 +72,7 @@ import { EscrowTransactionDetailComponent } from './components/escrow-transactio
 import { EscrowTableComponent } from './components/escrow-table/escrow-table.component';
 import { EscrowTransactionModule } from './components/escrow-transaction/escrow-transaction.module';
 import { MultisigTransactionModule } from './components/multisig-transaction/multisig-transaction.module';
+import { InputAddressModule } from './components/input-address/input-address.module';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
@@ -167,6 +168,7 @@ export function getLanguage(languageServ: LanguageService) {
     MatTabsModule,
     EscrowTransactionModule,
     MultisigTransactionModule,
+    InputAddressModule,
   ],
   bootstrap: [AppComponent],
   providers: [
---
+++ src/app/components/input-address/input-address.component.ts
@@ -0,0 +1,113 @@
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  NG_VALIDATORS,
+  FormControl,
+} from '@angular/forms';
+import { Contact, ContactService } from 'src/app/services/contact.service';
+import { AuthService, SavedAccount } from 'src/app/services/auth.service';
+import { isZBCAddressValid } from 'zoobc-sdk';
+@Component({
+  selector: 'input-address',
+  templateUrl: './input-address.component.html',
+  styleUrls: ['./input-address.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputAddressComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => InputAddressComponent),
+      multi: true,
+    },
+  ],
+})
+export class InputAddressComponent implements OnInit, ControlValueAccessor {
+  @Input() placeholder: string;
+  @Input() label: string;
+  @Input() classList: string;
+
+  value: string;
+  tempVal: string[];
+  contacts: Contact[];
+  contact: Contact;
+  account: SavedAccount;
+  accounts: SavedAccount[];
+  filteredContacts: Contact[];
+
+  private _onChange = (value: any) => {};
+  private _onTouched = (value: any) => {};
+  disabled: boolean;
+
+  constructor(
+    private contactServ: ContactService,
+    private authServ: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.contacts = this.contactServ.getList() || [];
+    this.getAccounts();
+  }
+
+  writeValue(value: any) {
+    this.value = value ? value : '';
+  }
+
+  registerOnChange(fn: any) {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this._onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    this.disabled = isDisabled;
+  }
+
+  onChange(value: any) {
+    this.filteredContacts = this.filterContacts(value);
+    this._onChange(value);
+  }
+
+  onOptionClick(address: any) {
+    this._onChange(address);
+  }
+
+  filterContacts(value: string): Contact[] {
+    if (value) {
+      const filterValue = value.toLowerCase();
+      return this.contacts.filter((contact: Contact) =>
+        contact.alias.toLocaleLowerCase().includes(filterValue)
+      );
+    } else if (value == '') return this.contacts;
+  }
+
+  getAccounts() {
+    this.accounts = this.authServ.getAllAccount();
+    this.accounts.forEach(account => {
+      const contact: Contact = {
+        address: account.address,
+        alias: account.name,
+      };
+      this.contacts.push(contact);
+    });
+  }
+
+  validate({ value }: FormControl) {
+    let result: boolean = false;
+    if (value) {
+      result = isZBCAddressValid(value);
+    } else {
+      return null;
+    }
+
+    if (!result) {
+      return { invalidAddress: true };
+    }
+    return null;
+  }
+}
