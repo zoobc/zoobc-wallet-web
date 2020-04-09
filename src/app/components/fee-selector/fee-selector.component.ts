df6bd6e910a96bfdba714e790f1931d0a28a4743
yusakman <yusakmanullang93@gmail.com>
2020-04-09 11:55:11 +0800

    Update Fee Selector Module
    update fee-selector module

--- src/app/components/fee-selector/fee-selector.component.ts
+++ src/app/components/fee-selector/fee-selector.component.ts
@@ -1,15 +1,116 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  CurrencyRateService,
+  Currency,
+} from 'src/app/services/currency-rate.service';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { environment } from 'src/environments/environment';
+import { truncate, calcMinFee } from 'src/helpers/utils';
 
 @Component({
   selector: 'app-fee-selector',
   templateUrl: './fee-selector.component.html',
-  styleUrls: ['./fee-selector.component.scss']
+  styleUrls: ['./fee-selector.component.scss'],
 })
 export class FeeSelectorComponent implements OnInit {
+  currencyRate: Currency;
 
-  constructor() { }
+  feeSlow = environment.fee;
+  feeMedium = this.feeSlow * 5;
+  feeFast = this.feeMedium * 5;
+  activeButton: number = 2;
+  kindFee: string;
 
-  ngOnInit() {
+  formSend: FormGroup;
+  recipientForm = new FormControl('', Validators.required);
+  amountForm = new FormControl('', [
+    Validators.required,
+    Validators.min(1 / 1e8),
+  ]);
+  amountCurrencyForm = new FormControl('', Validators.required);
+  feeForm = new FormControl(this.feeMedium, [
+    Validators.required,
+    Validators.min(this.feeSlow),
+  ]);
+  feeFormCurr = new FormControl('', Validators.required);
+  aliasField = new FormControl('', Validators.required);
+  addressApproverField = new FormControl('', Validators.required);
+  approverCommissionField = new FormControl('', [
+    Validators.required,
+    Validators.min(1 / 1e8),
+  ]);
+  approverCommissionCurrField = new FormControl('', [
+    Validators.required,
+    Validators.min(1 / 1e8),
+  ]);
+  instructionField = new FormControl('', Validators.required);
+  timeoutField = new FormControl('', [Validators.required, Validators.min(1)]);
+
+  typeCoin = 'ZBC';
+  typeFee = 'ZBC';
+  typeCommission = 'ZBC';
+
+  customFee: boolean = false;
+  advancedMenu: boolean = false;
+
+  constructor(
+    private currencyServ: CurrencyRateService,
+    public dialog: MatDialog
+  ) {
+    // disable alias field (saveAddress = false)
+    this.aliasField.disable();
   }
 
+  ngOnInit() {}
+
+  onChangeAmountField() {
+    const amount = truncate(this.amountForm.value, 8);
+    const amountCurrency = amount * this.currencyRate.value;
+    this.amountCurrencyForm.patchValue(amountCurrency);
+  }
+
+  onChangeAmountCurrencyField() {
+    const amount = this.amountCurrencyForm.value / this.currencyRate.value;
+    const amountTrunc = truncate(amount, 8);
+    this.amountForm.patchValue(amountTrunc);
+  }
+
+  onChangeFeeField() {
+    const fee = truncate(this.feeForm.value, 8);
+    const feeCurrency = fee * this.currencyRate.value;
+    this.feeFormCurr.patchValue(feeCurrency);
+  }
+
+  onChangeFeeCurrencyField() {
+    const fee = this.feeFormCurr.value / this.currencyRate.value;
+    const feeTrunc = truncate(fee, 8);
+    this.feeForm.patchValue(feeTrunc);
+  }
+
+  toggleCustomFee() {
+    this.customFee = !this.customFee;
+    if (!this.customFee) this.onFeeChoose(this.activeButton);
+  }
+
+  onFeeChoose(value) {
+    let fee: number = 0;
+    if (value === 1) {
+      fee = this.feeSlow;
+      this.kindFee = 'Slow';
+    } else if (value === 2) {
+      fee = this.feeMedium;
+      this.kindFee = 'Average';
+    } else {
+      fee = this.feeFast;
+      this.kindFee = 'Fast';
+    }
+
+    const feeCurrency = fee * this.currencyRate.value;
+    this.formSend.patchValue({
+      fee: fee,
+      feeCurr: feeCurrency,
+    });
+    this.activeButton = value;
+  }
 }
--- src/app/components/fee-selector/fee-selector.module.ts
+++ src/app/components/fee-selector/fee-selector.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeeSelectorComponent } from './fee-selector.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { DateAgoModule } from 'src/app/pipes/date-ago.module';
+import { HttpClient } from '@angular/common/http';
+import { MatCheckboxModule, MatSelectModule } from '@angular/material';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(
+    httpClient,
+    './assets/languages/locales/',
+    '.json'
+  );
+}
 
 @NgModule({
   declarations: [FeeSelectorComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    DateAgoModule,
+
+    MatCheckboxModule,
+    MatSelectModule,
+  ],
   exports: [FeeSelectorComponent],
 })
 export class FeeSelectorModule {}
