56d102532259fe1df22ad2cace7a40fb3041956f
bagasAgastya <bagasagastya19@gmail.com>
2020-04-24 17:09:20 +0800

    send transaction & some changes in confirm send

--- src/app/components/multisig-transaction/multisig-transaction.module.ts
+++ src/app/components/multisig-transaction/multisig-transaction.module.ts
@@ -5,21 +5,23 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { DateAgoModule } from 'src/app/pipes/date-ago.module';
 import { HttpClient } from '@angular/common/http';
-import { MatCardModule, MatButtonModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatDividerModule } from '@angular/material';
 import { AddressModule } from '../address/address.module';
+import { SendTransactionComponent } from './send-transaction/send-transaction.component';
+import { AccountSelectorModule } from '../account-selector/account-selector.module';
+import { FeeSelectorModule } from '../fee-selector/fee-selector.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(
-    httpClient,
-    './assets/languages/locales/',
-    '.json'
-  );
+  return new TranslateHttpLoader(httpClient, './assets/languages/locales/', '.json');
 }
 
 @NgModule({
-  declarations: [MultisigTransactionComponent],
+  declarations: [MultisigTransactionComponent, SendTransactionComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
@@ -30,7 +32,10 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     DateAgoModule,
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
     AddressModule,
+    AccountSelectorModule,
+    FeeSelectorModule,
   ],
   exports: [MultisigTransactionComponent],
 })
