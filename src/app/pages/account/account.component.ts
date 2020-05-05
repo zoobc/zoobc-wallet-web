e83132d3c5eff04dd3594cf4d1deb88d7f0988c4
yusakman <yusakmanullang93@gmail.com>
2020-05-05 18:18:54 +0800

    Update Accounts Page
    Update Accounts Page

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SavedAccount, AuthService } from 'src/app/services/auth.service';
-import zoobc, { getZBCAdress, TransactionListParams, toTransactionListWallet } from 'zoobc-sdk';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { MatDialog } from '@angular/material';
 import { MultisigInfoComponent } from './multisig-info/multisig-info.component';
-import { onCopyText } from 'src/helpers/utils';
+import { AddAccountComponent } from './add-account/add-account.component';
+import { EditAccountComponent } from './edit-account/edit-account.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -14,11 +15,36 @@ export class AccountComponent implements OnInit {
   currAcc: SavedAccount;
   accounts: SavedAccount[];
 
-  constructor(private authServ: AuthService, public dialog: MatDialog) {
+  constructor(private authServ: AuthService, public dialog: MatDialog, private router: Router) {
     this.currAcc = this.authServ.getCurrAccount();
+    this.accounts = this.authServ.getAllAccount();
   }
 
   ngOnInit() {}
+
+  onOpenAddAccount() {
+    this.dialog.open(AddAccountComponent, { width: '360px', maxHeight: '99vh' });
+  }
+
+  onOpenEditAccount(e, account: SavedAccount) {
+    e.stopPropagation();
+    const dialog = this.dialog.open(EditAccountComponent, {
+      width: '360px',
+      maxHeight: '99vh',
+      data: account,
+    });
+    dialog.afterClosed().subscribe((edited: boolean) => {
+      if (edited) {
+        this.accounts = this.authServ.getAllAccount();
+        this.currAcc = this.authServ.getCurrAccount();
+      }
+    });
+  }
+
+  onSwitchAccount(account: SavedAccount) {
+    this.authServ.switchAccount(account);
+    this.currAcc = account;
+  }
 
   onOpenMultisigInfoDialog() {
     this.dialog.open(MultisigInfoComponent, {
