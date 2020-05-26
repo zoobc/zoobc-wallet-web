5db93713274b41a2142ae33f0ea9e788d44a0e40
Bagas Agastya <43229728+bagasAgastya@users.noreply.github.com>
2020-05-26 08:42:24 +0800

    [FEATURE] My Task - Multisignature (#203)
    * Multisignature list and detail
    
    * Some changes
    
    * some changes

--- src/app/components/multisig-transaction/multisig-transaction.component.ts
+++ src/app/components/multisig-transaction/multisig-transaction.component.ts
@@ -1,13 +1,10 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  TemplateRef,
-  Input,
-} from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, Input, Output, EventEmitter } from '@angular/core';
 import Swal from 'sweetalert2';
 import { MatDialogRef, MatDialog } from '@angular/material';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from 'src/app/services/auth.service';
+import zoobc, { toGetPendingList } from 'zoobc-sdk';
+import { base64ToHex } from 'src/helpers/utils';
 
 @Component({
   selector: 'app-multisig-transaction',
@@ -15,15 +12,45 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./multisig-transaction.component.scss'],
 })
 export class MultisigTransactionComponent implements OnInit {
-  @ViewChild('detailMultisig') detailMultisig: TemplateRef<any>;
+  @ViewChild('detailMultisig') detailMultisigDialog: TemplateRef<any>;
+  @Input() pendingListMultiSig;
+  @Input() isLoading: boolean = false;
+  @Input() isError: boolean = false;
   @Input() withDetail: boolean = false;
+  @Output() refresh: EventEmitter<boolean> = new EventEmitter();
   detailMultisigRefDialog: MatDialogRef<any>;
-  constructor(public dialog: MatDialog, private translate: TranslateService) {}
 
-  ngOnInit() {}
+  multiSigDetail: any;
+  isLoadingDetail: boolean = false;
+  isLoadingTx: boolean = false;
+  account;
 
-  onOpenDetailTask() {
-    this.detailMultisigRefDialog = this.dialog.open(this.detailMultisig, {
+  constructor(public dialog: MatDialog, private translate: TranslateService, private authServ: AuthService) {}
+
+  ngOnInit() {
+    this.account = this.authServ.getCurrAccount();
+  }
+
+  onRefresh() {
+    this.refresh.emit(true);
+  }
+
+  openDetailMultiSignature(txHash) {
+    const hashHex = base64ToHex(txHash);
+    this.isLoadingDetail = true;
+    zoobc.MultiSignature.getPendingByTxHash(hashHex).then(res => {
+      const list = [];
+      list.push(res.pendingtransaction);
+      const tx = {
+        count: 1,
+        page: 1,
+        pendingtransactionsList: list,
+      };
+      const txFilter = toGetPendingList(tx);
+      this.multiSigDetail = txFilter.pendingtransactionsList[0];
+      this.isLoadingDetail = false;
+    });
+    this.detailMultisigRefDialog = this.dialog.open(this.detailMultisigDialog, {
       width: '500px',
     });
   }
@@ -41,9 +68,11 @@ export class MultisigTransactionComponent implements OnInit {
       timer: 1500,
     });
   }
+
   closeDialog() {
     this.detailMultisigRefDialog.close();
   }
+
   async onConfirmDialog() {
     this.detailMultisigRefDialog.close();
     let message: string;
