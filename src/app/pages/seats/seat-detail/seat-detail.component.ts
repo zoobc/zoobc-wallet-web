fef0ea8dd9069914aca9cd9bea95929c4a9f6f8d
Witarsana <made.witarsana@gmail.com>
2020-07-06 10:50:48 +0800

    Modify confirm-update component to display old and new data

--- src/app/pages/seats/seat-detail/seat-detail.component.ts
+++ src/app/pages/seats/seat-detail/seat-detail.component.ts
@@ -110,6 +110,14 @@ export class SeatDetailComponent implements OnInit {
     const confirmRefDialog = this.dialog.open(ConfirmUpdateComponent, {
       width: '800px',
       maxHeight: '90vh',
+      data: {
+        old: this.seat,
+        new: {
+          zbcAddress: this.addressField.value,
+          nodePubKey: this.nodePubKeyField.value,
+          message: this.messageField.value,
+        },
+      },
     });
 
     confirmRefDialog.afterClosed().subscribe(onConfirm => {
