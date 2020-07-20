4f9095bd12debac0ef3ebc07df4eb25f5963476f
Witarsana <made.witarsana@gmail.com>
2020-07-20 10:16:22 +0800

    create set up dataset component

---
+++ src/app/pages/account-dataset/setup-dataset/setup-dataset.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setup-dataset',
+  templateUrl: './setup-dataset.component.html',
+  styleUrls: ['./setup-dataset.component.scss']
+})
+export class SetupDatasetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
