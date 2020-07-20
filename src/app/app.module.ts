bfed6dee9f77a6da5a5f13a0a30f63e392ab432d
Witarsana <made.witarsana@gmail.com>
2020-07-20 11:18:30 +0800

    Listed in app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -211,6 +211,7 @@ export function getLanguage(languageServ: LanguageService) {
     ConfirmSendComponent,
     AddMultisigInfoComponent,
     MultisigInfoComponent,
+    SetupDatasetComponent,
   ],
 })
 export class AppModule {}
