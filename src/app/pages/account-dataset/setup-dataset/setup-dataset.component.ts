b525ea81c231f8bedda3189d8417b2d505a03ab0
Witarsana <made.witarsana@gmail.com>
2020-07-20 11:18:38 +0800

    Update setup-dataset.component.ts

--- src/app/pages/account-dataset/setup-dataset/setup-dataset.component.ts
+++ src/app/pages/account-dataset/setup-dataset/setup-dataset.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { SavedAccount } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-setup-dataset',
   templateUrl: './setup-dataset.component.html',
-  styleUrls: ['./setup-dataset.component.scss']
+  styleUrls: ['./setup-dataset.component.scss'],
 })
 export class SetupDatasetComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) private account: SavedAccount) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
