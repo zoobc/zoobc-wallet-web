fc67c4610ffb30dbaee6ebc64800d54bfe141d48
Made Witarsana <made.witarsana@gmail.com>
2020-07-21 03:11:20 +0800

    [FEATURE] Approval Multisig History Page (#277)
    * Add Multisig Approval History Component
    
    * Update app-routing.module.ts
    
    * get list and get detail
    
    * Update multisig-approval-history.component.ts
    
    * Update multisig-approval-history.component.ts
    
    * Move component
    
    * Change the route
    
    * Add link to multisig approval history on my task page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,6 +20,7 @@ import { AddMultisigInfoComponent } from './pages/multisignature/add-multisig-in
 import { CreateTransactionComponent } from './pages/multisignature/create-transaction/create-transaction.component';
 import { MultisignatureComponent } from './pages/multisignature/multisignature.component';
 import { AddParticipantsComponent } from './pages/multisignature/add-participants/add-participants.component';
+import { MultisigApprovalHistoryComponent } from './pages/my-task/multisig-approval-history/multisig-approval-history.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -42,7 +43,7 @@ const routes: Routes = [
       { path: 'multisignature/add-multisig-info', component: AddMultisigInfoComponent },
       { path: 'multisignature/create-transaction', component: CreateTransactionComponent },
       { path: 'multisignature/add-signatures', component: AddParticipantsComponent },
-
+      { path: 'multisig-approval-history', component: MultisigApprovalHistoryComponent },
       {
         path: 'settings',
         loadChildren: './pages/settings/settings.module#SettingsModule',
