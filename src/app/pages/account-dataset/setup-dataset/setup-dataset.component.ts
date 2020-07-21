5f6a094325c31283f0069fc5bb158c80e3b23b97
Witarsana <made.witarsana@gmail.com>
2020-07-21 13:10:59 +0800

    Add setup function from sdk

--- src/app/pages/account-dataset/setup-dataset/setup-dataset.component.ts
+++ src/app/pages/account-dataset/setup-dataset/setup-dataset.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Inject, ViewChild, OnDestroy } from '@angular/core';
 import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
-import { SavedAccount } from 'src/app/services/auth.service';
+import { SavedAccount, AuthService } from 'src/app/services/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { CurrencyRateService, Currency } from 'src/app/services/currency-rate.service';
 import { truncate } from 'src/helpers/utils';
 import { Subscription } from 'rxjs';
 import { PinConfirmationComponent } from 'src/app/components/pin-confirmation/pin-confirmation.component';
-
+import zoobc, { BIP32Interface, SetupDatasetResponse } from 'zoobc-sdk';
+import { SetupDatasetInterface } from 'zoobc-sdk/types/helper/transaction-builder/setup-account-dataset';
 @Component({
   selector: 'app-setup-dataset',
   templateUrl: './setup-dataset.component.html',
@@ -15,7 +16,6 @@ import { PinConfirmationComponent } from 'src/app/components/pin-confirmation/pi
 })
 export class SetupDatasetComponent implements OnInit, OnDestroy {
   subscription: Subscription = new Subscription();
-
   isSetupOther: boolean;
   isLoading: boolean;
   isError: boolean;
@@ -36,7 +36,8 @@ export class SetupDatasetComponent implements OnInit, OnDestroy {
     public dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) private account: SavedAccount,
     private currencyServ: CurrencyRateService,
-    private dialogRef: MatDialogRef<SetupDatasetComponent>
+    private dialogRef: MatDialogRef<SetupDatasetComponent>,
+    private authServ: AuthService
   ) {
     this.formGroup = new FormGroup({
       property: this.propertyField,
@@ -55,6 +56,7 @@ export class SetupDatasetComponent implements OnInit, OnDestroy {
     });
     this.subscription.add(subsRate);
     this.isSetupOther = false;
+    this.disableSetupOther();
   }
 
   ngOnDestroy() {
@@ -63,15 +65,39 @@ export class SetupDatasetComponent implements OnInit, OnDestroy {
 
   enableSetupOther() {
     this.recipientAddressField.enable();
-    this.feeForm.enable();
   }
 
   disableSetupOther() {
     this.recipientAddressField.disable();
-    this.feeForm.disable();
   }
 
-  setupDataset() {}
+  setupDataset() {
+    this.isError = false;
+    this.isLoading = true;
+    let receipient = this.recipientAddressField.value;
+    if (!this.isSetupOther) receipient = this.account.address;
+    const param: SetupDatasetInterface = {
+      property: this.propertyField.value,
+      value: this.valueField.value,
+      setterAccountAddress: this.account.address,
+      recipientAccountAddress: receipient,
+      fee: this.feeForm.value,
+    };
+    const keyring = this.authServ.keyring;
+    const seed = keyring.calcDerivationPath(this.account.path);
+    zoobc.AccountDataset.setupDataset(param, seed)
+      .then((res: SetupDatasetResponse) => {
+        this.dialogRef.close();
+      })
+      .catch(err => {
+        this.isError = true;
+        this.isLoading = false;
+        console.log(err);
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
 
   onClickSetupOther() {
     this.isSetupOther = !this.isSetupOther;
