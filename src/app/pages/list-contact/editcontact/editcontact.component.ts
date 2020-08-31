2976e48c4de3b15e4fb009005492abcdb9176704
Witarsana <made.witarsana@gmail.com>
2020-08-31 15:00:21 +0800

    Remove qr scanner and refresh recent transaction

--- src/app/pages/list-contact/editcontact/editcontact.component.ts
+++ src/app/pages/list-contact/editcontact/editcontact.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ContactService, Contact } from 'src/app/services/contact.service';
 import Swal from 'sweetalert2';
 import { TranslateService } from '@ngx-translate/core';
 import { isZBCAddressValid } from 'zoobc-sdk';
 import { getTranslation } from 'src/helpers/utils';
-import { QrScannerComponent } from '../../qr-scanner/qr-scanner.component';
 
 @Component({
   selector: 'app-editcontact',
@@ -21,8 +20,7 @@ export class EditcontactComponent implements OnInit {
     private contactServ: ContactService,
     public dialogRef: MatDialogRef<EditcontactComponent>,
     @Inject(MAT_DIALOG_DATA) public contact: Contact,
-    private translate: TranslateService,
-    private dialog: MatDialog
+    private translate: TranslateService
   ) {}
 
   onAddressValidation() {
@@ -54,24 +52,5 @@ export class EditcontactComponent implements OnInit {
         this.dialogRef.close(contacts);
       }
     }
-  }
-  openScannerForm() {
-    let address = this.addressField.value;
-    this.addressField.setValue('');
-    const dialogQr = this.dialog.open(QrScannerComponent, {
-      width: '480px',
-      maxHeight: '99vh',
-      data: 'string',
-      disableClose: true,
-    });
-    dialogQr.afterClosed().subscribe((data: any) => {
-      if (data) {
-        this.addressField.setValue(data[0]);
-        this.addressField.updateValueAndValidity();
-      } else {
-        this.addressField.setValue(address);
-        this.addressField.updateValueAndValidity();
-      }
-    });
   }
 }
