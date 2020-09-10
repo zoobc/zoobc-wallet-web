b7ef3caef2917c2f6ed80f19d534aa087e6592fe
Witarsana <made.witarsana@gmail.com>
2020-09-10 14:01:28 +0800

    Dont display import multisig account and add participant when txhash already generated

--- src/app/pages/multisignature/add-multisig-info/add-multisig-info.component.ts
+++ src/app/pages/multisignature/add-multisig-info/add-multisig-info.component.ts
@@ -49,6 +49,7 @@ export class AddMultisigInfoComponent implements OnInit, OnDestroy {
       if (multisigInfo === undefined) this.router.navigate(['/multisignature']);
 
       this.multisig = multisig;
+
       this.pushInitParticipant();
 
       if (multisigInfo) {
