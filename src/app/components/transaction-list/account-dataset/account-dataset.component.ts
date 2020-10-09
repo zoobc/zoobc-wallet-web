5adf5e71a88f306b0ebc507a9e4214d87fdeccc5
Bagas Agastya <43229728+bagasAgastya@users.noreply.github.com>
2020-10-09 08:48:22 +0800

    [FEATURE] Account Dataset List in Transaction History #426

---
+++ src/app/components/transaction-list/account-dataset/account-dataset.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ZBCTransaction } from 'zoobc-sdk';
+
+@Component({
+  selector: 'account-dataset',
+  templateUrl: './account-dataset.component.html',
+})
+export class AccountDatasetComponent implements OnInit {
+  @ViewChild('dialog') detailDialog: TemplateRef<any>;
+  @Input() transaction: ZBCTransaction;
+
+  constructor(private dialog: MatDialog) {}
+
+  ngOnInit() {}
+
+  openDetail(id) {
+    this.dialog.open(this.detailDialog, {
+      width: '500px',
+      maxHeight: '90vh',
+      data: id,
+    });
+  }
+}
