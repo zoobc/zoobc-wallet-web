81e29f364d9f341b2ff933020ebb7f9b0df49246
Witarsana <made.witarsana@gmail.com>
2020-10-21 16:05:39 +0800

    move escrow transaction and multisig transaction component

---
+++ src/app/pages/my-task/escrow-transaction/escrow-transaction.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EscrowTransactionComponent } from './escrow-transaction.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { DateAgoModule } from 'src/app/pipes/date-ago.module';
+import { HttpClient } from '@angular/common/http';
+import { MatCardModule, MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
+import { AddressModule } from 'src/app/components/address/address.module';
+import { FormEscrowApprovalModule } from 'src/app/components/transaction-form/form-escrow-approval/form-escrow-approval.module';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, './assets/languages/locales/', '.json');
+}
+
+@NgModule({
+  declarations: [EscrowTransactionComponent],
+  imports: [
+    CommonModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    DateAgoModule,
+    MatCardModule,
+    MatButtonModule,
+    AddressModule,
+    MatCheckboxModule,
+    MatInputModule,
+    FormEscrowApprovalModule,
+  ],
+  exports: [EscrowTransactionComponent],
+})
+export class EscrowTransactionModule {}
---
+++ src/app/pages/my-task/multisig-transaction/multisig-transaction.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MultisigTransactionComponent } from './multisig-transaction.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { DateAgoModule } from 'src/app/pipes/date-ago.module';
+import { HttpClient } from '@angular/common/http';
+import { MatCardModule, MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
+import { AddressModule } from 'src/app/components/address/address.module';
+import { InputAmountModule } from 'src/app/components/input-amount/input-amount.module';
+import { AccountSelectorModule } from 'src/app/components/account-selector/account-selector.module';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, './assets/languages/locales/', '.json');
+}
+
+@NgModule({
+  declarations: [MultisigTransactionComponent],
+  imports: [
+    CommonModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    DateAgoModule,
+    MatButtonModule,
+    MatCardModule,
+    AddressModule,
+    MatInputModule,
+    InputAmountModule,
+    MatCheckboxModule,
+    AccountSelectorModule,
+  ],
+  exports: [MultisigTransactionComponent],
+})
+export class MultisigTransactionModule {}
