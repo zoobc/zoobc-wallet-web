8f44d1f64ac86ebd4fcebe5fd28436b9e3b7cd20
Made Witarsana <made.witarsana@gmail.com>
2020-10-27 09:06:30 +0800

    [ENHANCEMENT] My Task - Re-route Escrow Approval History (#444)
    * [REDESIGN] Change Color Schema (#439)
    
    * change color schema
    
    * [REDESIGN] Change Button Style  (#440)
    
    * button style
    
    * code left behind
    
    * [FEATURE] Remodel Pending Transactions in transaction history page #441
    
    * re-route url
    
    * delete escrow transaction component
    
    * delete multisig transaction component
    
    * delete approval escrow history component
    
    * move escrow transaction and multisig transaction component
    
    * Update app module and routing
    
    * Revert "[REDESIGN] Change Color Schema (#439)"
    
    This reverts commit 667abb225bbf2133ada2ff2efd55e1d08edea656.
    
    * Revert "[REDESIGN] Change Button Style  (#440)"
    
    This reverts commit 972d466b7ed115ddc26decd93c9500736b9fcb10.
    
    * update css
    
    Co-authored-by: Gung De Surya <gungde.avenger@gmail.com>
    Co-authored-by: Bagas Agastya <43229728+bagasAgastya@users.noreply.github.com>

--- src/app/pages/my-task/multisig-transaction/multisig-transaction.module.ts
+++ src/app/pages/my-task/multisig-transaction/multisig-transaction.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EscrowTransactionComponent } from './escrow-transaction.component';
+import { MultisigTransactionComponent } from './multisig-transaction.component';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { DateAgoModule } from 'src/app/pipes/date-ago.module';
 import { HttpClient } from '@angular/common/http';
 import { MatCardModule, MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
-import { AddressModule } from '../address/address.module';
-import { FormEscrowApprovalModule } from 'src/app/components/transaction-form/form-escrow-approval/form-escrow-approval.module';
+import { AddressModule } from 'src/app/components/address/address.module';
+import { InputAmountModule } from 'src/app/components/input-amount/input-amount.module';
+import { AccountSelectorModule } from 'src/app/components/account-selector/account-selector.module';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient, './assets/languages/locales/', '.json');
 }
 
 @NgModule({
-  declarations: [EscrowTransactionComponent],
+  declarations: [MultisigTransactionComponent],
   imports: [
     CommonModule,
     TranslateModule.forChild({
@@ -25,13 +26,14 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
       },
     }),
     DateAgoModule,
-    MatCardModule,
     MatButtonModule,
+    MatCardModule,
     AddressModule,
-    MatCheckboxModule,
     MatInputModule,
-    FormEscrowApprovalModule,
+    InputAmountModule,
+    MatCheckboxModule,
+    AccountSelectorModule,
   ],
-  exports: [EscrowTransactionComponent],
+  exports: [MultisigTransactionComponent],
 })
-export class EscrowTransactionModule {}
+export class MultisigTransactionModule {}
