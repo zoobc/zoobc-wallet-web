444f7134bd7d9b724ea00c801f73f54470f57454
gungdesurya <gungde.avenger@gmail.com>
2021-01-14 16:20:24 +0800

    fixing get transactions bug

--- src/app/pages/transferhistory/transferhistory.component.ts
+++ src/app/pages/transferhistory/transferhistory.component.ts
@@ -5,13 +5,9 @@ import zoobc, {
   TransactionListParams,
   MempoolListParams,
   TransactionType,
-  EscrowListParams,
-  OrderBy,
   ZBCTransaction,
-  getZBCAddress,
   Address,
   ZBCTransactions,
-  parseAddress,
 } from 'zbc-sdk';
 
 import { ContactService } from 'src/app/services/contact.service';
@@ -84,72 +80,28 @@ export class TransferhistoryComponent implements OnDestroy {
       };
 
       try {
-        let trxList = await zoobc.Transactions.getList(txParam);
-
-        let lastHeight = 0;
-        let firstHeight = 0;
-        if (trxList.transactions.length > 0) {
-          lastHeight = trxList.transactions[0].height;
-          firstHeight = trxList.transactions[trxList.transactions.length - 1].height;
-        }
-
-        const multisigTx = trxList.transactions.filter(trx => trx.multisig == true).map(trx => trx.id);
-
-        const paramEscrow: EscrowListParams = {
-          blockHeightStart: firstHeight,
-          blockHeightEnd: lastHeight,
-          recipient: this.address,
-          statusList: [0, 1, 2, 3],
-          latest: false,
-          pagination: {
-            orderBy: OrderBy.DESC,
-            orderField: 'block_height',
-          },
-        };
-        this.startMatch = 0;
-        const escrowTx = await zoobc.Escrows.getList(paramEscrow);
-
-        const escrowList = escrowTx.escrowList;
-        const escrowGroup = this.groupEscrowList(escrowList);
-
-        let txs = trxList.transactions;
-        txs.map(recent => {
-          let escStatus = this.matchEscrowGroup(recent.height, escrowGroup);
-          recent.senderAlias = this.contactServ.get(recent.sender.value).name || '';
-          recent.recipientAlias = this.contactServ.get(recent.recipient.value).name || '';
-          const nodeManagementTxType =
-            this.txType == 2 || this.txType == 258 || this.txType == 514 || this.txType == 770;
-          if (nodeManagementTxType) {
-            const hasNodePublicKey = recent.txBody.nodepublickey;
-            const hasAccountAddress = recent.txBody.accountaddress;
-            if (hasNodePublicKey) {
-              const buffer = Buffer.from(recent.txBody.nodepublickey.toString(), 'base64');
-              const pubkey = getZBCAddress(buffer, 'ZNK');
-              recent['txBody'].nodepublickey = pubkey;
-            }
-            if (hasAccountAddress) {
-              const accountAddress = parseAddress(recent.txBody.accountaddress);
-              recent['txBody'].accountaddress = accountAddress.value;
-            }
-          }
-          if (escStatus) {
-            recent.escrow = true;
-            recent['txBody'].approval = escStatus.status;
-          } else recent.escrow = false;
-          recent.multisig = multisigTx.includes(recent.id);
-          return recent;
+        const trxList = await zoobc.Transactions.getList(txParam);
+        trxList.transactions.map(transaction => {
+          transaction.senderAlias = this.contactServ.get(transaction.sender.value).name || '';
+          transaction.recipientAlias = this.contactServ.get(transaction.recipient.value).name || '';
+          return transaction;
         });
         this.total = trxList.total;
-        this.accountHistory = reload ? txs : this.accountHistory.concat(txs);
+        this.accountHistory = reload
+          ? trxList.transactions
+          : this.accountHistory.concat(trxList.transactions);
 
         if (reload) {
-          const mempoolParams: MempoolListParams = { address: this.address };
-          this.unconfirmTx = await zoobc.Mempool.getList(mempoolParams).then((res: ZBCTransactions) =>
-            res.transactions.map(uc => {
-              this.txType = uc.transactionType;
-              return uc;
-            })
-          );
+          const mempoolParams: MempoolListParams = { address: this.address, txType: this.txType };
+          const unconfirmTx = await zoobc.Mempool.getList(mempoolParams);
+          console.log(unconfirmTx);
+
+          unconfirmTx.transactions.map(transaction => {
+            transaction.senderAlias = this.contactServ.get(transaction.sender.value).name || '';
+            transaction.recipientAlias = this.contactServ.get(transaction.recipient.value).name || '';
+            return transaction;
+          });
+          this.unconfirmTx = unconfirmTx.transactions;
         }
       } catch {
         this.isError = true;
