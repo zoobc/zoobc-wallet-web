6df30b5ce1a62701f83ab4b454a2bad00bb202ce
zaenury <adhiimzaenury@gmail.com>
2021-02-22 11:04:01 +0800

    add GPLv3 license on root

--- src/app/pages/settings/network/network.component.ts
+++ src/app/pages/settings/network/network.component.ts
@@ -43,7 +43,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-network',
   templateUrl: './network.component.html',
-  styleUrls: ['./network.component.scss'],
 })
 export class NetworkComponent implements OnInit {
   constructor() {}
